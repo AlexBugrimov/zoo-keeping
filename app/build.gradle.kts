17be2cef320f69e8755e20ebaf641017c5f72cc1
Alex Bugrimov <dev@bugrimov.ru>
2023-10-30 23:00:25 +0300

    ➕[#1] Added Either and libs.versions.toml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     java
-    id("org.springframework.boot") version "3.1.5"
-    id("io.spring.dependency-management") version "1.1.3"
+    alias(libs.plugins.springframework.boot)
+    alias(libs.plugins.spring.dependency.management)
 }
 
 java {
@@ -13,6 +13,7 @@ repositories {
 }
 
 dependencies {
+    implementation(libs.either.java)
     implementation("org.springframework.boot:spring-boot-starter-web")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
--- common/src/main/java/dev/bug/either/ExceptionThrowingSupplier.java
+++
@@ -1,6 +0,0 @@
-package dev.bug.either;
-
-@FunctionalInterface
-public interface ExceptionThrowingSupplier<T> {
-    T get() throws Exception;
-}
